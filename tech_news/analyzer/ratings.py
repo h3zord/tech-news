003df32e0a04d6011af50d459d359af62bdea0f3
h3zord <lucaschmain@gmail.com>
2023-03-15 12:28:04 -0300

    Completing requirement 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,10 @@
+from tech_news.database import find_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    category_list = Counter(
+        sorted([new["category"] for new in news])).most_common(5)
+    return [category[0] for category in category_list]
