c0009eda162d21b5c9c82ebfc80111acdab6f6d9
h3zord <lucaschmain@gmail.com>
2023-03-15 11:38:57 -0300

    Completing requirement 07 and 08

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,26 @@
+from tech_news.database import search_news
+import datetime
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    return [
+        (news["title"], news["url"])
+        for news in search_news({"title": {"$regex": title, "$options": "i"}})
+    ]
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        date_format = datetime.fromisoformat(date).strftime("%d/%m/%Y")
+        return [
+            (news["title"], news["url"])
+            for news in search_news({"timestamp": date_format})
+        ]
+
+    except ValueError:
+        raise ValueError('Data inválida')
 
 
 # Requisito 9
