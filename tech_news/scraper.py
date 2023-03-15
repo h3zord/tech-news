5f1dde0ed4471b156f49d7f5433a3a1ebc607334
h3zord <lucaschmain@gmail.com>
2023-03-14 21:23:50 -0300

    Completing requirement 04

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 from parsel import Selector
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -28,12 +29,33 @@ def scrape_updates(html_content):
 def scrape_next_page_link(html_content):
     selector = Selector(text=html_content)
     url = selector.css("div.nav-links a.next::attr(href)").get()
+
     return url if url else None
 
 
 # Requisito 4
-def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+def scrape_news(html_content: Selector):
+    selector = Selector(text=html_content)
+
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css("h1.entry-title::text").get().strip()
+    timestamp = selector.css("ul.post-meta li.meta-date::text").get()
+    writer = selector.css("span.author a.url::text").get()
+    reading_time = int((selector.css(
+        "li.meta-reading-time::text").get()).split(" ")[0])
+    summary = "".join(selector.css(
+        "div.entry-content > p:first-of-type *::text").getall()).strip()
+    category = selector.css("span.label::text").get()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": reading_time,
+        "summary": summary,
+        "category": category
+    }
 
 
 # Requisito 5
